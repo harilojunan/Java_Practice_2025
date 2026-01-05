b7f6aadfd3bdc96a1289650d4ed9011aaa7345fd
Harilojunan <hari23may@gmail.com>
2026-01-06 00:05:51 +0530

    second largest final

--- src/dsaPractices/FindTheSecondLargestNumber.java
+++ src/dsaPractices/FindTheSecondLargestNumber.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FindTheSecondLargestNumber {
-
+	
 	public static void main(String[] args) {
+		
 		List<Integer> nums = Arrays.asList(25,36,54,78,25,6,5,47,29);
 		
+//		Better approach BigO(2n) 
 		int largest = nums.get(0);
-		int slargest = -1;
+		int smallest = nums.get(0);
+		int seclargest = -1;
+		int secsmallest = 100;
 		
 		for(int i=0;i<nums.size();i++) {
 			if(nums.get(i) > largest) {
@@ -18,13 +22,38 @@ public class FindTheSecondLargestNumber {
 		}
 		
 		for(int j=0;j<nums.get(j);j++) {
-			if(nums.get(j) > slargest && nums.get(j) != largest) {
-				slargest = nums.get(j);
+			if(nums.get(j) > seclargest && nums.get(j) != largest) {
+				seclargest = nums.get(j);
 			}
 		}
 		
-		System.out.println(slargest);
-			
+		System.out.println(seclargest);
+		
+//		Optimal approach for second largest BigO(n)
+		for(int i=1; i< nums.size();i++) {
+			if(nums.get(i) > largest) {
+				seclargest = largest;
+				largest = nums.get(i);
+			}
+			else {
+				if(nums.get(i) < largest && nums.get(i) > seclargest) {
+					seclargest = nums.get(i);
+				}
+			}
+		}
+		System.out.println("The Second largest is " + seclargest);	
+		
+//		Optimal approach for second largest BigO(n)
+		for(int i=0; i<nums.size(); i++) {
+			if(nums.get(i) < smallest) {
+				secsmallest = smallest;
+				smallest = nums.get(i);
+			}
+			else if(nums.get(i) != smallest && nums.get(i) < secsmallest) {
+				secsmallest = nums.get(i);
+			}
+		}
+		System.out.println("The Second smallest is " + secsmallest);
 	}
 
 }
